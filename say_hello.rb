eed173dcc6cafe0ddcba786097688318ffe949e9
Sara Roybal <s.roybal13@gmail.com>
2020-08-25 05:10:33 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -3,12 +3,3 @@ def say_hello(name)
   puts "Hello #{name}!"
 end
 
-def say_hello
-  puts "Hello Ruby Programmer!"
-end
-
-def say_hello(name = "Ruby Programmer!")
-  puts "Hello #{name}!"
-end
-
-say_hello("Gabriela!")
