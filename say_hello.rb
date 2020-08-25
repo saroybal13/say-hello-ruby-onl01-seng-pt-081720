9c467c1288c01e9f73142e672ea76077ebea53ae
Sara Roybal <s.roybal13@gmail.com>
2020-08-25 05:03:58 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,11 +1,9 @@
 # Build your say_hello method here
-def say_hello(name)
+def say_hello("Gabriela")
   puts "Hello #{name}!"
 end
 
 
-def say_hello(name = "Hello Ruby Programmer!")
+def say_hello(name = "Ruby Programmer!")
   puts "Hello #{name}!"
 end
-
-say_hello
